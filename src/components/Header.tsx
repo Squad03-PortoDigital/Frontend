29fc8227493beb32ce8be0fa23b0a2be8acf9abd
Antony Rafael <antonyrafaelsouzaa@gmail.com>
2025-09-18 11:49:27 -0300

    pesquisa e logout do header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,38 @@
 import "../styles/header.css";
 import LogoBrancaFlap from "../images/LogoBrancaFlap.png";
+import { LogOut, Menu, Search } from "lucide-react";
+import { InputAdornment, TextField } from "@mui/material";
 
 export default function Header() {
     return (
         <div className="header-container">
             <div className="header">
                 <div className="logo-header-flap"><img src={LogoBrancaFlap} alt="Logo" /></div>
-                <div className="pesquisa-header"></div>
-                <div className="logout-header"></div>
+                <div className="pesquisa-header">
+                    <TextField
+                        fullWidth
+                        placeholder="Pesquisar"
+                        variant="outlined"
+                        InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                            <Menu size={20} strokeWidth={3} />
+                            </InputAdornment>
+                        ),
+                        endAdornment: (
+                            <InputAdornment position="end">
+                            <Search size={20} strokeWidth={3} />
+                            </InputAdornment>
+                        ),
+                        style: {
+                            backgroundColor: "#cbd5f7", // cor de fundo do input
+                            borderRadius: "8px",
+                            width: "500px",
+                        },
+                        }}
+                    />
+                </div>
+                <div className="logout-header"> <LogOut size={36} color="#fff"/> </div>
             </div>
         </div>
     );
