f593715703effee6601eb7196b9937b126ae2729
Antony Rafael <antonyrafaelsouzaa@gmail.com>
2025-09-18 12:52:36 -0300

    estrutura inicial do menu lateral

---
+++ src/components/Menu.tsx
@@ -0,0 +1,25 @@
+export default function Menu() {
+    return (
+        <div className="menu-container">
+            <div className="menu">
+                <div className="menu-kanban">
+                    <div className="menu-tela-inicial"></div>
+                    <div className="menu-tela-arquivados"></div>
+                    <div className="menu-tela-finalizados"></div>
+                </div>
+                <div className="menu-informacoes">
+                    <div className="menu-calendario"></div>
+                    <div className="menu-notificacoes"></div>
+                </div>
+                <div className="menu-gestor">
+                    <div className="menu-tela-dashboard"></div>
+                    <div className="menu-tela-equipe"></div>
+                </div>
+                <div className="menu-mais">
+                    <div className="menu-configuracoes"></div>
+                    <div className="menu-ajuda"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
