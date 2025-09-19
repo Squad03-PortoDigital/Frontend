90ab186204efe6435da86f83e37f244c863fff63
Antony Rafael <antonyrafaelsouzaa@gmail.com>
2025-09-19 11:59:14 -0300

    Versão inicial do menu lateral

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,4 @@
+import { Archive, Bell, Calendar, ChartPie, House, Info, Settings, SquareCheckBig, Users } from "lucide-react";
 import "../styles/menu.css";
 
 export default function Menu() {
@@ -5,25 +6,66 @@ export default function Menu() {
         <div className="menu-container">
             <div className="menu">
                 <div className="menu-perfil">
-                    <div className="menu-perfil-imagem"></div>
-                    <div className="menu-perfil-nome"></div>
+                    <div className="menu-perfil-item">
+                        <div className="menu-perfil-imagem"></div>
+                        <div className="menu-perfil-nome">Perfil</div>
+                    </div>
                 </div>
-                <div className="menu-kanban">
-                    <div className="menu-tela-inicial"></div>
-                    <div className="menu-tela-arquivados"></div>
-                    <div className="menu-tela-finalizados"></div>
+
+                <div className="menu-section">
+                    <h2 className="menu-item-titulo">Kanban</h2>
+                    <div className="menu-item">
+                        <House size={28} />
+                        <div className="menu-item-nome">Tela inicial</div>
+                    </div>
+                    <div className="menu-item">
+                        <Archive size={28} />
+                        <div className="menu-item-nome">Arquivados</div>
+                    </div>
+                    <div className="menu-item">
+                        <SquareCheckBig size={28} />
+                        <div className="menu-item-nome">Finalizados</div>
+                    </div>
                 </div>
-                <div className="menu-informacoes">
-                    <div className="menu-calendario"></div>
-                    <div className="menu-notificacoes"></div>
+
+                <div className="menu-section">
+                    <h2 className="menu-item-titulo">Informações</h2>
+                    <div className="menu-item">
+                        <Calendar size={24} />
+                        <div className="menu-item-nome">Calendário</div>
+                    </div>
+                    <div className="menu-item">
+                        <Bell size={24} />
+                        <div className="menu-item-nome">Notificações</div>
+                    </div>
                 </div>
-                <div className="menu-gestor">
-                    <div className="menu-tela-dashboard"></div>
-                    <div className="menu-tela-equipe"></div>
+
+                <div className="menu-section">
+                    <h2 className="menu-item-titulo">Gestor</h2>
+                    <div className="menu-item">
+                        <ChartPie size={24} />
+                        <div className="menu-item-nome">Dashboard</div>
+                    </div>
+                    <div className="menu-item">
+                        <Users size={24} />
+                        <div className="menu-item-nome">Equipe</div>
+                    </div>
                 </div>
-                <div className="menu-mais">
-                    <div className="menu-configuracoes"></div>
-                    <div className="menu-ajuda"></div>
+
+                <div className="menu-section">
+                    <h2 className="menu-item-titulo">Mais</h2>
+                    <div className="menu-item">
+                        <Settings size={24} />
+                        <div className="menu-item-nome">Configurações</div>
+                    </div>
+                    <div className="menu-item">
+                        <Info size={24} />
+                        <div className="menu-item-nome">Ajuda</div>
+                    </div>
+                </div>
+
+                <div className="menu-final">
+                    Flap - All Rights Reserved © 2025
                 </div>
             </div>
         </div>
