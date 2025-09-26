61190a4082e1214128c20542e8b8c06389a15778
HugoGomes <151577426+HugoGomesz@users.noreply.github.com>
2025-09-26 12:15:29 -0300

    refactor: change struct

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import Header from "./components/Header"
-import Menu from "./components/Menu"
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Menu from "./components/Menu";
+import Header from "./components/Header";
 
 function App() {
-
   return (
-    <>
-      <Menu />
-      <Header />
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<><Menu /><Header /></>} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
